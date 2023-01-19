79bc38d826c6c078215fff40396093a779eeb472
SwAurora <rlatjddnr74@gmail.com>
2023-01-20 03:01:32 +0900

    2023-01-20 master(ver1)

--- src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
+++ src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
@@ -1,19 +1,19 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepositoryV1;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JpaConfig
+public class SpringDataJpaConfig
 {
-    private final EntityManager em;
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
 
     @Bean
     public ItemService itemService()
@@ -24,6 +24,6 @@ public class JpaConfig
     @Bean
     public ItemRepository itemRepository()
     {
-        return new JpaItemRepositoryV1(em);
+        return new JpaItemRepositoryV2(springDataJpaItemRepository);
     }
 }
